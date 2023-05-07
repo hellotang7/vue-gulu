cee4d37e6eae62623b8acd782bb309e669478e53
hellotang7 <2714148252@qq.com>
2023-05-07 21:28:50 +0800

    实现路由切换之后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script lang="ts">
 
     import {provide, ref} from "vue";
+    import {router} from "./router";
 
-export default {
-  name: 'App',
-setup(){
-      //获取页面宽度决定asideVisible
-      const width = document.documentElement.clientWidth
-    console.log(width)
-      const asideVisible = ref(width <= 500 ? false:true)
-    provide('asideVisible',asideVisible) //set
-}
-}
+    export default {
+        name: 'App',
+        setup() {
+            //获取页面宽度决定asideVisible
+            const width = document.documentElement.clientWidth
+            console.log(width)
+            const asideVisible = ref(width <= 500 ? false : true)
+            provide('asideVisible', asideVisible) //set
+            router.afterEach(() => {
+                if (width <= 500) {
+                    asideVisible.value = false
+                }
+            })
+
+        }
+    }
 </script>
