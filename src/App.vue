ac45bf24c9e0fcb1f1d292a85d9bb60921fa1fcf
hellotang7 <2714148252@qq.com>
2023-05-07 20:44:38 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <router-view></router-view>
 </template>
 
-<script>
+<script lang="ts">
+
+    import {provide, ref} from "vue";
 
 export default {
   name: 'App',
-
+setup(){
+      const asideVisible = ref(false)
+    provide('xxx',asideVisible) //set
+}
 }
 </script>
