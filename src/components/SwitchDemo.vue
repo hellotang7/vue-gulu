cb6b0446fa5990078b26badf2830dffd922a9ff4
hellotang7 <2714148252@qq.com>
2023-05-08 22:30:57 +0800

    使用v-model实现按钮点击切换

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,19 @@
 <template>
-    <div><Switch/></div>
+    <div>
+        <Switch v-model:value="y"/>
+    </div>
 
 </template>
 
 <script>
     import Switch from "../lib/Switch.vue";
+    import {ref} from "vue";
     export default {
-        components:{Switch}
+        components:{Switch},
+        setup(){
+            const y = ref(false)
+            return {y}
+        }
     }
 </script>
 
