019206a701a8115e4c8cddbf99b1330075371d03
hellotang7 <2714148252@qq.com>
2023-05-11 22:01:04 +0800

    indicator css

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,18 +1,28 @@
 <template>
     <div>Tabs 示例</div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs  v-model:selected="x">
         <tab title="导航1">内容1</tab>
         <tab title="导航2">内容2</tab>
     </Tabs>
 </template>
 
 <script lang="ts">
+    import {
+        ref
+    } from 'vue'
     import Tabs from '../lib/Tabs.vue'
     import Tab from '../lib/Tab.vue'
     export default {
-        components:{Tabs,Tab}
+        components:{Tabs,Tab},
+        setup() {
+            const x = ref('导航1')
+            return {
+                x
+            }
+        }
     }
+
 </script>
 
 <style scoped>
