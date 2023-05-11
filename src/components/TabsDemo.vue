308fbc82acdb659a567789a283deb4f47d378abc
hellotang7 <2714148252@qq.com>
2023-05-11 23:32:52 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     <h1>示例1</h1>
     <Tabs  v-model:selected="x">
         <tab title="导航1">内容1</tab>
-        <tab title="导航2">内容2</tab>
+        <tab title="导航22222222">内容2</tab>
     </Tabs>
 </template>
 
@@ -16,7 +16,7 @@
     export default {
         components:{Tabs,Tab},
         setup() {
-            const x = ref('导航1')
+            const x = ref('导航22222222')
             return {
                 x
             }
